ac09242267cabd41ccd861398087c9d1fe8cf805
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-12-05 02:12:45 -0300

    Analisis nutricional

--- NutriBalance/NutriBalance/EdanamApi/Constants.cs
+++ NutriBalance/NutriBalance/EdanamApi/Constants.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NutriBalance.EdamanApi
+namespace NutriBalance.EdanamApi
 {
     class Constants
     {
--- NutriBalance/NutriBalance/MainPage.xaml.cs
+++ NutriBalance/NutriBalance/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using NutriBalance.ViewModel;
-using NutriBalance.EdamanApi;
+using NutriBalance.EdanamApi;
 
 namespace NutriBalance
 {
