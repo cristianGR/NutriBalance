4b567916265fc1a13bf6fc5b47872269fd0692ea
cristian grimaldi <grimaldi.cristian@gmail.com>
2019-12-08 22:46:14 -0300

    Primer paso para app definitiva de análisis nutricional

--- NutriBalance/NutriBalance/MainPage.xaml.cs
+++ NutriBalance/NutriBalance/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 using NutriBalance.ViewModel;
+using NutriBalance.View;
 using NutriBalance.EdanamApi;
 
 namespace NutriBalance
@@ -16,12 +17,13 @@ namespace NutriBalance
     public partial class MainPage : ContentPage
     {
         RestService _restService;
+        
         public MainPage()
         {
             InitializeComponent();
             _restService = new RestService();
             BindingContext = new MainPageViewModel();
-            
+            Title = "ALIMENTOS";
         }
 
         string GenerateRequestUri(string endpoint, string ingredients)
@@ -31,24 +33,28 @@ namespace NutriBalance
             return requestUri;
         }
 
-
         private async void Item_Tapped(object sender, EventArgs e)
         {
             var item = sender as ViewCell;
+
             Label ingredient = (Label)item.FindByName("lblIngredient");
-            await DisplayAlert("ENVIANDO...", "Análisis  para : 100 gr "  + ingredient.Text, "Aceptar");
 
-            NutritionalFacts nutritionalFactsData= await _restService.GetNutritionalData(GenerateRequestUri(Constants.EdanamAPIEndPoind, "100 gr " + ingredient.Text));
+            listView.SelectedItem = false;
 
-            await DisplayAlert("RESPUESTA", 
-                " Kcal: " + nutritionalFactsData.calories + "\n" +
-                " Calcio: " + nutritionalFactsData.totalNutrients._CA.quantity+ " " +
-                nutritionalFactsData.totalNutrients._CA.unit + "\n"+
-                " Vitamina C: " + nutritionalFactsData.totalNutrients._VITC.quantity + " " +
-                nutritionalFactsData.totalNutrients._VITC.unit + "\n" +
-                " Hierro: " + nutritionalFactsData.totalNutrients._FE.quantity + " " +
-                nutritionalFactsData.totalNutrients._FE.unit ,
-                "Aceptar");
+            _timeToresponse.IsRunning = true;
+            _timeToresponse.IsVisible = true;
+
+            NutritionalFacts nutritionalFactsData = await _restService.GetNutritionalData(GenerateRequestUri(Constants.EdanamAPIEndPoind, "100 gr " + ingredient.Text));
+
+            _timeToresponse.IsRunning = false;
+            _timeToresponse.IsVisible = false;
+
+            await Navigation.PushAsync(new NutritionalDataView()
+            {
+                Title = ingredient.Text,
+                BindingContext = nutritionalFactsData.totalNutrients
+
+            }) ;
 
         }
     }
